25d72dbc5925561f77a74913358ea389b8251b14
swedge <scott.wedge@gmail.com>
2020-03-13 23:05:17 -0400

    Fixes #1

--- lms/djangoapps/instructor/management/tests/test_compute_grades.py
+++ lms/djangoapps/instructor/management/tests/test_compute_grades.py
@@ -20,7 +20,7 @@ class InstructorCommandsTest(TestCase):
         offline_grade_calculation = Mock()  # pylint: disable=redefined-outer-name
         CourseKey.from_string = Mock(return_value=CourseLocator(*course_id.split('/')))
         call_command('compute_grades', )
-        self.asertEqual(offline_grade_calculation.call_count, 1)  # pylint: disable=no-member
+        self.assertEqual(offline_grade_calculation.call_count, 1)  # pylint: disable=no-member
         offline_grade_calculation.assert_called_with(CourseKey.from_string('MITx/0.0001/2016_Fall'))
 
     def test_compute_grades_command_multiple_courses(self):
@@ -29,6 +29,6 @@ class InstructorCommandsTest(TestCase):
         CourseKey.from_string = Mock()
         offline_grade_calculation = Mock()  # pylint: disable=redefined-outer-name
         call_command('compute_grades', '{0} {1}'.format(course_id1, course_id1))
-        self.asertEqual(offline_grade_calculation.call_count, 2)  # pylint: disable=no-member
+        self.assertEqual(offline_grade_calculation.call_count, 2)  # pylint: disable=no-member
         CourseKey.from_string.assert_called_with(course_id1)
         CourseKey.from_string.assert_called_with(course_id2)
